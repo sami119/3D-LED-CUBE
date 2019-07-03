c4b0e417326cbb9b132239c31e63d12176d14bac
SlavKazakov <35624858+SlavKazakov@users.noreply.github.com>
2019-07-03 23:31:42 +0300

    Version 1.0.7
    added symbol animation

--- skeleton/skeleton.ino
+++ skeleton/skeleton.ino
@@ -17,9 +17,11 @@
 //times
 #define RAIN_TIME 260
 #define GLOW_TIME 8
+#define SYMBOL_TIME 300
 
 //shifts
 #define NEG_Y 0
+#define NEG_Z 3
 
 //axis
 #define AXIS_X 0
@@ -39,7 +41,7 @@ void setup() {
   Serial.begin(9600);
   SPI.begin();
   SPI.beginTransaction(SPISettings(8000000, MSBFIRST, SPI_MODE0));
-  
+
   //pins setup
   pinMode(RED_LED, OUTPUT);
   pinMode(GREEN_LED, OUTPUT);
@@ -55,59 +57,116 @@ void setup() {
 
 void loop() {
   //buttons tick
-  if(digitalRead(BUTTON_1)){
+  if (digitalRead(BUTTON_1)) {
     currentEffect ++;
     loading = true;
     clearCube();
 
-    if(currentEffect >= TOTAL){
+    if (currentEffect >= TOTAL) {
       currentEffect = 0;
     }
-    
+
     loadingLeds();
-  } 
-  
-  if(digitalRead(BUTTON_2)){
+  }
+
+  if (digitalRead(BUTTON_2)) {
     currentEffect --;
     loading = true;
     clearCube();
-    
-    if(currentEffect < 0){
+
+    if (currentEffect < 0) {
       currentEffect = TOTAL - 1;
     }
-    
+
     loadingLeds();
   }
-  
+
   //change effects
-  switch(currentEffect){
+  switch (currentEffect) {
     //case LIGHTCUBE: lightCube(); break;
     case RAIN: rain(); break;
     case PLANEBOING: planeBoing(); break;
     case SYMBOL: symbol(); break;
-    case GLOW: glow(); break; 
+    case GLOW: glow(); break;
   }
-  
+
   renderCube();
 }
 
 //Animations
 //RAIN animation
-void rain(){
-  if(loading){
+void rain() {
+  if (loading) {
     clearCube();
     loading = false;
   }
-  
+
   timer++;
-  
-  if(timer > RAIN_TIME){
+
+  if (timer > RAIN_TIME) {
     uint8_t numDrops = random(0, 5);
     timer = 0;
     shift(NEG_Y);
-    
-    for(uint8_t i = 0; i < numDrops; i++){
-      setVoxel(random(0,8), 7, random(0,8));
+
+    for (uint8_t i = 0; i < numDrops; i++) {
+      setVoxel(random(0, 8), 7, random(0, 8));
+    }
+  }
+}
+
+//Symbol Animation 'Database'
+uint8_t characters[][8] = //small library with different symbols inside
+{
+  {0x3C, 0x40, 0x40, 0x3C, 0x40, 0x40, 0x42, 0x3C},//3
+  {0x3C, 0x42, 0x40, 0x40, 0x3C, 0x02, 0x02, 0x7E},//2
+  {0x10, 0x18, 0x14, 0x10, 0x10, 0x10, 0x10, 0x3C},//1
+  {0x66, 0xFF, 0xFF, 0xFF, 0xFF, 0x7E, 0x3C, 0x18} //<3
+};
+uint8_t charCounter = 0;
+uint8_t charPosition = 0;
+
+//Symbol animation
+void symbol() {
+  //variables
+  char string[] {};
+  uint8_t len;
+
+  if (loading)
+  {
+    clearCube();
+    charPosition = -1; //sets position before the the 0 location in the cube
+    charCounter = 0; //begins the chars in the characters 'database'
+    loading = false;
+  }
+  timer++; //begins timer
+  if (timer > SYMBOL_TIME)
+  {
+    timer = 0;
+
+    shift(NEG_Z);
+    charPosition++;
+
+    if (charPosition == 7)
+    {
+      /*when the position of the char is at the end of the cube
+        it begins the animation movement again
+      */
+
+      charCounter++;
+      if (charCounter > len - 1)
+      {
+        charCounter = 0;
+      }
+      charPosition = 0;
+
+    }
+    //movement of the character
+    if (charPosition == 0)
+    {
+      for (uint8_t i = 0; i < 8; i++)
+      {
+        cube[i][0] = characters[string[charCounter] - '0'][i];
+      }
     }
   }
 }
@@ -115,44 +174,40 @@ void rain(){
 //Plane Boing Animation
 int planeCounter;
 
-void planeBoing(){
-  if(loading){
+void planeBoing() {
+  if (loading) {
     clearCube();
-    planeCounter=0;
+    planeCounter = 0;
     loading = false;
   }
-  
-  if(planeCounter == AXIS_X){
+
+  if (planeCounter == AXIS_X) {
     planeBoing(AXIS_X);
   }
-  
-  if(planeCounter == AXIS_Y){
+
+  if (planeCounter == AXIS_Y) {
     planeBoing(AXIS_Y);
   }
-  
-  if(planeCounter == AXIS_Z){
+
+  if (planeCounter == AXIS_Z) {
     planeBoing(AXIS_Z);
   }
 }
 
-void planeBoing(int plane){
-  for(int i = 0; i < 8; i++){
+void planeBoing(int plane) {
+  for (int i = 0; i < 8; i++) {
     fill(0x00);
     setPlane(plane, i);
-    delay(500); 
+    delay(500);
   }
-  
-  for (int i=7;i>=0;i--){
+
+  for (int i = 7; i >= 0; i--) {
     fill(0x00);
-    setPlane(plane,i);
+    setPlane(plane, i);
     delay(500);
   }
 }
 
-//Symbol animation
-void symbol(){
-  
-}
 
 //Glowing animation
 bool glowing;
@@ -163,45 +218,45 @@ uint8_t selX = 0;
 uint8_t selY = 0;
 uint8_t selZ = 0;
 
-void glow(){
+void glow() {
   if (loading) {
     clearCube();
     glowCount = 0;
     glowing = true;
     loading = false;
   }
-  
+
   timer++;
- 
-  if(timer > GLOW_TIME){
+
+  if (timer > GLOW_TIME) {
     timer = 0;
-    
-    if(glowing){
-      if(glowCount < 256){
-        do{
-          randomLED();    
+
+    if (glowing) {
+      if (glowCount < 256) {
+        do {
+          randomLed();
         }
         while (getVoxel(selX, selY, selZ));
-        
+
         setVoxel(selX, selY, selZ);
-        glowCount++; 
+        glowCount++;
       }
-      else{
-      glowing = false;
-      glowCount = 0;
+      else {
+        glowing = false;
+        glowCount = 0;
       }
     }
-    else{
-      if(glowCount < 256){
-        do{
-           randomLED();
+    else {
+      if (glowCount < 256) {
+        do {
+          randomLed();
         }
         while (!getVoxel(selX, selY, selZ));
-        
+
         clearVoxel(selX, selY, selZ);
-        glowCount++; 
+        glowCount++;
       }
-      else{
+      else {
         clearCube();
         glowing = true;
         glowCount = 0;
@@ -210,16 +265,16 @@ void glow(){
   }
 }
 
-
-void randomLed(){
-  selX = random(0,8);
-  selY = random(0,8);
-  selZ = random(0,8);   
+void randomLed() {
+  selX = random(0, 8);
+  selY = random(0, 8);
+  selZ = random(0, 8);
 }
 
+
 /*
-//all leds shine
-void lightCube() {
+  //all leds shine
+  void lightCube() {
   if (loading) {
     clearCube();
     loading = false;
@@ -229,16 +284,16 @@ void lightCube() {
       cube[i][j] = 0xFF;
     }
   }
-}
+  }
 */
 
 //Main methods used in the loop
 
 //fills the cube with pattern of 8bits
-void fill(uint8_t pattern){
-  for(int y = 0; y<8; y++){
-    for(int z = 0; z<8; z++){
-      cube[y][z] = pattern;  
+void fill(uint8_t pattern) {
+  for (int y = 0; y < 8; y++) {
+    for (int z = 0; z < 8; z++) {
+      cube[y][z] = pattern;
     }
   }
 }
@@ -256,16 +311,16 @@ void renderCube() {
 }
 
 //Clears the cube
-void clearCube(){
-  for(uint8_t i = 0; i < 8; i++){
-   for(uint8_t j = 0; j < 8; j++){
-    cube[i][j] = 0; 
-   }
+void clearCube() {
+  for (uint8_t i = 0; i < 8; i++) {
+    for (uint8_t j = 0; j < 8; j++) {
+      cube[i][j] = 0;
+    }
   }
 }
 
 //When loading: the leds are shining red.
-void loadingLeds(){
+void loadingLeds() {
   timer = 0;
   digitalWrite(RED_LED, HIGH);
   digitalWrite(GREEN_LED, LOW);
@@ -275,9 +330,9 @@ void loadingLeds(){
 }
 
 //Voxel control
-//shines the led by given y, z and x 
-void setVoxel(uint8_t x, uint8_t y, uint8_t z){
-  cube[7-y][7-z] != (0x01 << x);
+//shines the led by given y, z and x
+void setVoxel(uint8_t x, uint8_t y, uint8_t z) {
+  cube[7 - y][7 - z] != (0x01 << x);
 }
 
 //clears the led by given x, y and z
@@ -291,21 +346,28 @@ bool getVoxel(uint8_t x, uint8_t y, uint8_t z) {
 }
 
 //shifts the lights in the given direction
-void shift(uint8_t dir){
-  if(dir == NEG_Y){
-    for(uint8_t y = 7; y > 0; y--){
-      for(uint8_t z = 1; z < 8; z++){
+void shift(uint8_t dir) {
+  if (dir == NEG_Y) {
+    for (uint8_t y = 7; y > 0; y--) {
+      for (uint8_t z = 1; z < 8; z++) {
         cube[y][z] = cube[y - 1][z];
+      }
+    }
+  }
+  else if (dir == NEG_Z) {
+    for (uint8_t y = 0; y < 8; y++) {
+      for (uint8_t z = 7; z > 0; z--) {
+        cube[y][z] = cube[y][z - 1];
       }
     }
   }
 }
 
 //sets a plane of 8x8 bits to light
-void setPlane(int axis, uint8_t i){
+void setPlane(int axis, uint8_t i) {
   for (uint8_t j = 0; j < 8; j++) {
     for (uint8_t k = 0; k < 8; k++) {
-      switch(axis){
+      switch (axis) {
         case AXIS_X: setVoxel(i, j, k); break;
         case AXIS_Y: setVoxel(j, i, k); break;
         case AXIS_Z: setVoxel(j, k, i); break;
