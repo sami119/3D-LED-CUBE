bca54a80b6cfd6cf14379f2036ee7096e81b7c58
Samuil <35292702+sami119@users.noreply.github.com>
2019-07-06 04:06:36 +0300

    Version 2.0.0
    working on:
    debuging some animations
    pattern
    WORKING VERSION!!!
    
    Co-Authored-By: stefanstefo <stefanstefo@users.noreply.github.com>
    Co-Authored-By: SlavKazakov <slavkazakov@users.noreply.github.com>

--- skeleton/skeleton.ino
+++ skeleton/skeleton.ino
@@ -1,20 +1,21 @@
 #include <SPI.h>
 
 //pins
-#define RED_LED 5
-#define GREEN_LED 7
+#define RED_LED 14
+#define GREEN_LED 15
 #define BUTTON_1 18
 #define BUTTON_2 19
 
 //animations
-//#define LIGHTCUBE 0
-#define RAIN 0
-#define PLANEBOING 1
-#define SYMBOL 2
-#define GLOW 3
-#define SEND_VOXELS 4
-#define WOOP_WOOP 5
-#define TOTAL 6
+#define LIGHTCUBE 0
+#define RAIN 1
+#define PLANEBOING 2
+#define SYMBOL 3
+#define GLOW 4
+#define SEND_VOXELS 5
+#define WOOP_WOOP 6
+#define PATERN 7
+#define TOTAL 8
 
 //times
 #define RAIN_TIME 260
@@ -22,14 +23,16 @@
 #define SYMBOL_TIME 300
 #define SEND_VOXELS_TIME 140
 #define WOOP_WOOP_TIME 350
+#define PLANE_BOING_TIME 120
 
 //shifts
-#define NEG_Y 0
-#define NEG_Z 1
-#define NEG_X 2
-#define POS_X 3
+#define POS_X 0
+#define NEG_X 1
+#define POS_Y 2
+#define NEG_Y 3
 #define POS_Z 4
-#define POS_Y 5
+#define NEG_Z 5
+
 
 //axis
 #define AXIS_X 0
@@ -45,8 +48,8 @@ uint8_t selY = 0;
 uint8_t selZ = 0;
 
 //main variables
-int8_t currentEffect;
 uint8_t cube[8][8];
+int8_t currentEffect;
 bool loading;
 
 void setup() {
@@ -55,6 +58,9 @@ void setup() {
   SPI.begin();
   SPI.beginTransaction(SPISettings(8000000, MSBFIRST, SPI_MODE0));
 
+  loading = true;
+  currentEffect = LIGHTCUBE;
+
   //pins setup
   pinMode(RED_LED, OUTPUT);
   pinMode(GREEN_LED, OUTPUT);
@@ -62,15 +68,12 @@ void setup() {
   pinMode(BUTTON_2, INPUT_PULLUP);
 
   randomSeed(analogRead(0));
-
-  currentEffect = RAIN;
   digitalWrite(GREEN_LED, HIGH);
-  loading = false;
 }
 
 void loop() {
   //buttons tick
-  if (digitalRead(BUTTON_1)) {
+  if (digitalRead(BUTTON_1) == LOW) {
     currentEffect ++;
     loading = true;
     clearCube();
@@ -82,7 +85,7 @@ void loop() {
     loadingLeds();
   }
 
-  if (digitalRead(BUTTON_2)) {
+  if (digitalRead(BUTTON_2) == LOW) {
     currentEffect --;
     loading = true;
     clearCube();
@@ -93,18 +96,34 @@ void loop() {
 
     loadingLeds();
   }
+
   //change effects
   switch (currentEffect) {
-    //case LIGHTCUBE: lightCube(); break;
+    case LIGHTCUBE: lightCube(); break;
     case RAIN: rain(); break;
     case PLANEBOING: planeBoing(); break;
-    case SYMBOL: symbol(); break;
+    case SYMBOL: symbol("3210", 4); break;
     case GLOW: glow(); break;
     case SEND_VOXELS: sendVoxels(); break;
     case WOOP_WOOP: woopWoop(); break;
+    case PATERN: pattern(); break;
   }
 
   renderCube();
+}
+
+void renderCube() {
+  for (uint8_t i = 0; i < 8; i++) {
+    digitalWrite(SS, LOW);
+    SPI.transfer(0x01 << i);
+
+    for (uint8_t j = 0; j < 8; j++) {
+      SPI.transfer(cube[i][j]);
+    }
+
+    digitalWrite(SS, HIGH);
+    //delay(1);
+  }
 }
 
 //Animations
@@ -123,29 +142,25 @@ void rain() {
     shift(NEG_Y);
 
     for (uint8_t i = 0; i < numDrops; i++) {
-
-      (random(0, 8), 7, random(0, 8));
+      setVoxel(random(0, 8), 7, random(0, 8));
     }
   }
 }
 
 //Symbol Animation 'Database'
-uint8_t characters[][8] = //small library with different symbols inside
+uint8_t characters[4][8] = //small library with different symbols inside
 {
-  {0x3C, 0x40, 0x40, 0x3C, 0x40, 0x40, 0x42, 0x3C},//3
-  {0x3C, 0x42, 0x40, 0x40, 0x3C, 0x02, 0x02, 0x7E},//2
-  {0x10, 0x18, 0x14, 0x10, 0x10, 0x10, 0x10, 0x3C},//1
-  {0x66, 0xFF, 0xFF, 0xFF, 0xFF, 0x7E, 0x3C, 0x18} //<3
+  {0x0E, 0x11, 0x10, 0x0C, 0x10, 0x10, 0x11, 0x0E}, //3
+  {0x3C, 0x42, 0x40, 0x40, 0x3C, 0x02, 0x02, 0x7E}, //2
+  {0x10, 0x18, 0x14, 0x10, 0x10, 0x10, 0x10, 0x3C}, //1
+  {0x66, 0xFF, 0xFF, 0xFF, 0xFF, 0x7E, 0x3C, 0x18}, //<3
 };
+
 uint8_t charCounter = 0;
 uint8_t charPosition = 0;
 
 //Symbol animation
-void symbol() {
-  //variables
-  char string[]{};
-  uint8_t num = 4;
-
+void symbol(char str[], uint8_t num) {
   if (loading)
   {
     clearCube();
@@ -153,7 +168,9 @@ void symbol() {
     charCounter = 0; //begins the chars in the characters 'database'
     loading = false;
   }
+
   timer++; //begins timer
+
   if (timer > SYMBOL_TIME)
   {
     timer = 0;
@@ -174,69 +191,73 @@ void symbol() {
       }
 
       charPosition = 0;
-
     }
+
     //movement of the character
     if (charPosition == 0)
     {
       for (uint8_t i = 0; i < 8; i++)
       {
-        cube[i][0] = characters[string[charCounter] - '0'][i];
+        cube[i][0] = characters[str[charCounter] - '0'][i];
       }
     }
   }
 }
 
+//SEND VOXELS animation
 uint8_t sendDirection = 0;
 bool sending = false;
 
 void sendVoxels() {
   if (loading) {
     clearCube();
-    loading = false;
 
     for (uint8_t x = 0; x < 8; x++) {
-      for (uint8_t y = 0; y < 8; y++) {
-        setVoxel(x, y, random(0, 2) * 7);
+      for (uint8_t z = 0; z < 8; z++) {
+        setVoxel(x, random(0, 2) * 7, z);
       }
     }
+
+    loading = false;
   }
 
   timer++;
+
   if (timer > SEND_VOXELS_TIME) {
     timer = 0;
 
     if (!sending) {
       selX = random(0, 8);
-      selY = random(0, 8);
+      selZ = random(0, 8);
 
-      if (getVoxel(selX, selY, 0)) {
-        selZ = 0;
-        sendDirection = NEG_Z;
+      if (getVoxel(selX, 0, selZ)) {
+        selY = 0;
+        sendDirection = POS_Y;
+
       }
-      else if (getVoxel(selX, selY, 7)) {
-        selZ = 7;
-        sendDirection = POS_Z;
+      else if (getVoxel(selX, 7, selZ)) {
+        selY = 7;
+        sendDirection = NEG_Y;
       }
 
       sending = true;
     }
     else {
-      if (sendDirection == POS_Z) {
-        selZ++;
+      if (sendDirection == POS_Y) {
+        selY++;
         setVoxel(selX, selY, selZ);
-        clearVoxel(selX, selY, selZ - 1);
+        clearVoxel(selX, selY - 1, selZ);
 
-        if (selZ == 7) {
+        if (selY == 7) {
           sending = false;
         }
       }
       else {
-        selZ--;
+        selY--;
         setVoxel(selX, selY, selZ);
-        clearVoxel(selX, selY, selZ + 1);
+        clearVoxel(selX, selY + 1, selZ);
 
-        if (selZ == 0) {
+        if (selY == 0) {
           sending = false;
         }
       }
@@ -246,14 +267,14 @@ void sendVoxels() {
 
 //variables for WoopWoop animation
 uint8_t cubeSize = 0;
-bool CubeExpanding = true;
+bool cubeExpanding = true;
 
 //woop woop animation
 void woopWoop() {
   if (loading) {
     clearCube();
     cubeSize = 2;
-    CubeExpanding = true;
+    cubeExpanding = true;
     loading = false;
   }
 
@@ -265,11 +286,11 @@ void woopWoop() {
   if (timer > WOOP_WOOP_TIME) {
     timer = 0;
 
-    if (CubeExpanding) {
+    if (cubeExpanding) {
       cubeSize += 2;
 
       if (cubeSize == 8) {
-        CubeExpanding = false;
+        cubeExpanding = false;
       }
     }
     /*Starts decreasing the size of the cube until the size of 2 and then
@@ -279,7 +300,7 @@ void woopWoop() {
       cubeSize -= 2;
 
       if (cubeSize == 2) {
-        CubeExpanding = true;
+        cubeExpanding = true;
       }
     }
 
@@ -289,42 +310,85 @@ void woopWoop() {
 }
 
 //Plane Boing Animation
-int planeCounter;
+uint8_t planePosition = 0;
+uint8_t planeDirection = 0;
+bool looped = false;
 
 void planeBoing() {
   if (loading) {
-    clearCube();
-    planeCounter = 0;
+    uint8_t axis = random(0, 3);
+    planePosition = random(0, 2) * 7;
+    timer = 0;
+    looped = false;
     loading = false;
-  }
 
-  if (planeCounter == AXIS_X) {
-    planeBoing(AXIS_X);
+    clearCube();
+    setPlane(axis, planePosition);
+    setPlaneDirection(axis);
   }
+  timer++;
 
-  if (planeCounter == AXIS_Y) {
-    planeBoing(AXIS_Y);
-  }
+  if (timer > PLANE_BOING_TIME) {
+    timer = 0;
+    shift(planeDirection);
 
-  if (planeCounter == AXIS_Z) {
-    planeBoing(AXIS_Z);
+    if (planeDirection % 2 == 0) {
+      planePosition++;
+
+      if (planePosition == 7) {
+        if (looped) {
+          loading = true;
+        }
+        else {
+          planeDirection++;
+          looped = true;
+        }
+      }
+    }
+    else {
+      planePosition--;
+
+      if (planePosition == 0) {
+        if (looped) {
+          loading = true;
+        }
+        else {
+          planeDirection--;
+          looped = true;
+        }
+      }
+    }
   }
 }
 
-void planeBoing(int plane) {
-  for (int i = 0; i < 8; i++) {
-    fill(0x00);
-    setPlane(plane, i);
-    delay(500);
-  }
-
-  for (int i = 7; i >= 0; i--) {
-    fill(0x00);
-    setPlane(plane, i);
-    delay(500);
+void setPlaneDirection(uint8_t axis) {
+  switch (axis) {
+    case AXIS_X:
+      if (planePosition == 0) {
+        planeDirection = POS_X;
+      }
+      else {
+        planeDirection = NEG_X;
+      }
+      break;
+    case AXIS_Y:
+      if (planePosition == 0) {
+        planeDirection = POS_Y;
+      }
+      else {
+        planeDirection = NEG_Y;
+      }
+      break;
+    case AXIS_Z:
+      if (planePosition == 0) {
+        planeDirection = POS_Z;
+      }
+      else {
+        planeDirection = NEG_Z;
+      }
+      break;
   }
 }
-
 
 //Glowing animation
 bool glowing;
@@ -337,14 +401,13 @@ void glow() {
     glowing = true;
     loading = false;
   }
-
   timer++;
 
   if (timer > GLOW_TIME) {
     timer = 0;
 
     if (glowing) {
-      if (glowCount < 256) {
+      if (glowCount < 448) {
         do {
           randomLed();
         }
@@ -352,26 +415,35 @@ void glow() {
 
         setVoxel(selX, selY, selZ);
         glowCount++;
-      }
-      else {
-        glowing = false;
-        glowCount = 0;
-      }
-    }
-    else {
-      if (glowCount < 256) {
-        do {
-          randomLed();
-        }
-        while (!getVoxel(selX, selY, selZ));
 
-        clearVoxel(selX, selY, selZ);
-        glowCount++;
+        if (glowCount < 512) {
+          for (uint8_t i = 0; i < 8; i++) {
+            for (uint8_t j = 0; j < 8; j++) {
+              cube[i][j] = 0xFF;
+            }
+          }
+          glowCount++;
+        }
+        else {
+          glowing = false;
+          glowCount = 0;
+        }
       }
       else {
-        clearCube();
-        glowing = true;
-        glowCount = 0;
+        if (glowCount < 448) {
+          do {
+            randomLed();
+          }
+          while (!getVoxel(selX, selY, selZ));
+
+          clearVoxel(selX, selY, selZ);
+          glowCount++;
+        }
+        else {
+          clearCube();
+          glowing = true;
+          glowCount = 0;
+        }
       }
     }
   }
@@ -384,41 +456,55 @@ void randomLed() {
 }
 
 
-/*
-  //all leds shine
-  void lightCube() {
+//lightCube animation
+void lightCube() {
+  if (loading) {
+    clearCube();
+    for (uint8_t i = 0; i < 8; i++) {
+      for (uint8_t j = 0; j < 8; j++) {
+        cube[i][j] = 0xFF;
+      }
+    }
+    loading = false;
+  }
+}
+
+//pattern animation
+bool full;
+
+void pattern() {
   if (loading) {
     clearCube();
     loading = false;
+    full = false;
   }
-  for(uint8_t i=0; i<8; i++) {
-    for(uint8_t j=0; j<8; j++) {
-      cube[i][j] = 0xFF;
-    }
+  if (full) {
+    fill(0x24);
+    delay(500);
+    fill(0x42);
+    delay(500);
+    full = false;
   }
+  else {
+    fill(0x81);
+    delay(500);
+    fill(0x42);
+    delay(500);
+    fill(0x24);
+    delay(500);
+    fill(0x18);
+    delay(500);
+    full = true;
   }
-*/
+}
 
 //Main methods used in the loop
-
 //fills the cube with pattern of 8bits
 void fill(uint8_t pattern) {
   for (int y = 0; y < 8; y++) {
     for (int z = 0; z < 8; z++) {
       cube[y][z] = pattern;
     }
-  }
-}
-
-//renders the array of bits to lights
-void renderCube() {
-  for (uint8_t i = 0; i < 8; i++) {
-    digitalWrite(SS, LOW);//begins transfering data
-    SPI.transfer(0x01 << (7 - i));//y counts from top to bottom
-    for (uint8_t j = 0; j < 8; j++) {
-      SPI.transfer(cube[i][j]);
-    }
-    digitalWrite(SS, HIGH);//closes conection
   }
 }
 
@@ -444,7 +530,7 @@ void loadingLeds() {
 //Voxel control
 //shines the led by given y, z and x
 void setVoxel(uint8_t x, uint8_t y, uint8_t z) {
-  cube[7 - y][7 - z] != (0x01 << x);
+  cube[7 - y][7 - z] |= (0x01 << x);
 }
 
 //clears the led by given x, y and z
@@ -459,17 +545,10 @@ bool getVoxel(uint8_t x, uint8_t y, uint8_t z) {
 
 //shifts the lights in the given direction
 void shift(uint8_t dir) {
-  if (dir == NEG_Y) {
-    for (uint8_t y = 7; y > 0; y--) {
-      for (uint8_t z = 1; z < 8; z++) {
-        cube[y][z] = cube[y - 1][z];
-      }
-    }
-  }
-  else if (dir == NEG_Z) {
+  if (dir == POS_X) {
     for (uint8_t y = 0; y < 8; y++) {
-      for (uint8_t z = 7; z > 0; z--) {
-        cube[y][z] = cube[y][z - 1];
+      for (uint8_t z = 0; z < 8; z++) {
+        cube[y][z] = cube[y][z] << 1;
       }
     }
   }
@@ -480,31 +559,50 @@ void shift(uint8_t dir) {
       }
     }
   }
-  else if (dir == POS_X) {
-    for (uint8_t y = 0; y < 8; y++) {
-      for (uint8_t z = 0; z < 8; z++) {
-        cube[y][z] = cube[y][z] << 1;
-      }
-    }
-  }
-  else if (dir == POS_Z) {
-    for (uint8_t y = 0; y < 8; y++) {
-      for (uint8_t z = 1; z < 8; z++) {
-        cube[y][z - 1] = cube[y][z];
-      }
-    }
-    for (uint8_t i = 0; i < 8; i++) {
-      cube[i][7] = 0;
-    }
-  }
+
   else if (dir == POS_Y) {
     for (uint8_t y = 1; y < 8; y++) {
       for (uint8_t z = 0; z < 8; z++) {
         cube[y - 1][z] = cube[y][z];
       }
     }
+
     for (uint8_t i = 0; i < 8; i++) {
       cube[7][i] = 0;
+    }
+  }
+  else if (dir == NEG_Y) {
+    for (uint8_t y = 7; y > 0; y--) {
+      for (uint8_t z = 0; z < 8; z++) {
+        cube[y][z] = cube[y - 1][z];
+      }
+    }
+
+    for (uint8_t i = 0; i < 8; i++) {
+      cube[0][i] = 0;
+    }
+  }
+
+  else if (dir == POS_Z) {
+    for (uint8_t y = 0; y < 8; y++) {
+      for (uint8_t z = 1; z < 8; z++) {
+        cube[y][z - 1] = cube[y][z];
+      }
+    }
+
+    for (uint8_t i = 0; i < 8; i++) {
+      cube[i][7] = 0;
+    }
+  }
+  else if (dir == NEG_Z) {
+    for (uint8_t y = 0; y < 8; y++) {
+      for (uint8_t z = 7; z > 0; z--) {
+        cube[y][z] = cube[y][z - 1];
+      }
+    }
+
+    for (uint8_t i = 0; i < 8; i++) {
+      cube[i][0] = 0;
     }
   }
 }
